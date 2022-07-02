606a978e5bfbcc7ab518c5728b0eac95701fc37a
teekey1991 <matsunaetaiki@gmail.com>
2022-07-02 11:25:00 +0900

    Hello Owner追加

--- 01.py
+++ 01.py
@@ -6,6 +6,13 @@ app = Flask(__name__)
 def hello_world():
     return "Hello,World"
 
+@app.route("/owner")
+def owner():
+    return "Hello Owner"
+
+@app.route("/owner/<name>")
+def owner_name(name):
+    return f"Hello {name}"
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
